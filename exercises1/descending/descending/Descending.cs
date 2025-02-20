ba71d7cac8ced9d7fe88843ebb57c0f8db2dbf7c
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-02-20 14:28:15 +0100

    Add files via upload

---
+++ exercises1/descending/descending/Descending.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace descending
+{
+    internal class Descending
+    {
+        static void Main()
+        {
+            Console.WriteLine(SortDigitsDescending(3581));
+        }
+
+        static int SortDigitsDescending(int num)
+        {
+            return int.Parse(new string(num.ToString().OrderByDescending(c => c).ToArray()));
+        }
+    }
+}
---
+++ exercises1/prime/prime/Prime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    internal class Prime
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (IsPrime(number))
+                    Console.WriteLine($"{number} is a prime number.");
+                else
+                    Console.WriteLine($"{number} is not a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is neither a prime, nor a composite number.");
+            }
+        }
+
+        static bool IsPrime(int num)
+        {
+            if (num < 2) return false;
+            if (num == 2) return true;
+            if (num % 2 == 0) return false;
+
+            for (int i = 3; i * i <= num; i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
