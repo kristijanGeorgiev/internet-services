2f4e64a38fc41efdc585f34eb0e1256e3b54f856
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-02-20 14:24:23 +0100

    Add files via upload

---
+++ exercises1/descending/descending/Descending.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace descending
+{
+    internal class Descending
+    {
+        static void Main()
+        {
+            Console.WriteLine(DescendingOrder(42145));
+            Console.WriteLine(DescendingOrder(145263));
+            Console.WriteLine(DescendingOrder(123456789));
+        }
+
+        static int DescendingOrder(int num)
+        {
+            return int.Parse(new string(num.ToString().OrderByDescending(c => c).ToArray()));
+        }
+    }
+}
---
+++ exercises1/reverse/reverse/Reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    internal class Reverse
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseString(input);
+
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
