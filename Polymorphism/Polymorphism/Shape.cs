7067248635db38f840b230e97da1c42f098b0ad3
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-13 00:51:35 +0100

    Add files via upload

---
+++ Polymorphism/Polymorphism/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Polymorphism
+{
+    class Shape
+    {
+        public virtual double CalculateArea()
+        {
+            return 0.0;
+        }
+    }
+    class Circle: Shape
+    {
+        public double radius { get; set; }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+    class Rectangle: Shape
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Polymorphism.Polymorphism;
+
+Shape circle = new Circle(6.3);
+Shape rectangle = new Rectangle(4.2, 8.6);
+Console.WriteLine($" Circle Area is {circle.CalculateArea()}");
+Console.WriteLine($" Rectangle Area ia {rectangle.CalculateArea()}");
