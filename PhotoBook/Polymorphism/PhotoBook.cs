84117f87b6cf7b09285d3f7c929d3331c6b4f599
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-13 16:41:56 +0100

    Add files via upload

---
+++ PhotoBook/Polymorphism/PhotoBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBook.Polymorphism
+{
+    class Book
+    {
+        public int numPages { get; set; }
+        public Book()
+        {
+            numPages = 16;
+        }
+        public Book(int numPages)
+        {
+            this.numPages = numPages;
+        }
+        public virtual int GetNumPages()
+        {
+            return numPages;
+        }
+    }
+    class BigPhotoBook : Book
+    {
+        public BigPhotoBook(int numPages) : base(numPages)
+        {
+            this.numPages = numPages;
+        }
+        public BigPhotoBook()
+        {
+            this.numPages = 15;
+        }
+        public override int GetNumPages()
+        {
+            return numPages;
+        }
+    }
+}
---
+++ PhotoBook/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region PhotoBook
+using PhotoBook.Polymorphism;
+Book PhotoBook = new Book(62);
+Book PhotoBook1 = new Book();
+BigPhotoBook bigPhotoBook2 = new BigPhotoBook(84);
+BigPhotoBook bigPhotoBook3 = new BigPhotoBook();
+Console.WriteLine(PhotoBook1.GetNumPages());
+Console.WriteLine(PhotoBook.GetNumPages());
+Console.WriteLine(bigPhotoBook2.GetNumPages());
+Console.WriteLine(bigPhotoBook3.GetNumPages());
+#endregion
