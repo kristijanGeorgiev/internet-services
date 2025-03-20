277045feea4b80e5680635b69a433c1366a0e307
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-20 18:21:49 +0100

    Add files via upload

---
+++ car/Classes/Car.cs
@@ -0,0 +1,27 @@
+﻿using car.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car.Classes
+{
+    public class Car : IVehiculo
+    {
+        public int amount { get; set; }
+        public Car(int amount)
+        {
+            this.amount = amount;
+        }
+        public void Drive()
+        {
+            Console.WriteLine("The Car Is Driving");
+        }
+        public bool Refuel(int amount)
+        {
+            amount = amount + 1;
+            return true;
+        }
+    }
+}
---
+++ car/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Interfaces
+using car.Classes;
+
+Car car = new Car(120);
+Console.WriteLine("Enter amount of car ");
+int amount = int.Parse(Console.ReadLine());
+car.Drive();
+#endregion
