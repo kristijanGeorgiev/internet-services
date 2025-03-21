f899b649dee911b6eca3dc63b2754d73a2415515
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-21 19:00:59 +0100

    Add files via upload

---
+++ class diagram/Polymorphism/Shape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Polymorphism
+{
+    class Shape
+    {
+        public virtual double CalculateArea()
+        {
+            return 0.0;
+        }
+        public virtual double CalculatePerimeter()
+        {
+            return 0.0;
+        }
+    }
+    class Circle : Shape
+    {
+        public double radius { get; set; }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return 3.14 * radius * radius;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * 3.14 * radius;
+        }
+    }
+    class Rectangle : Shape
+    {
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public override double CalculateArea()
+        {
+            return side1 * side2;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * side1 + 2 * side2;
+        }
+    }
+}
---
+++ class diagram/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Polymorphism.Polymorphism;
+Shape circle = new Circle(6.3);
+Shape rectangle = new Rectangle(4.2, 8.6);
+Console.WriteLine($" Circle Area is {circle.CalculateArea()}");
+Console.WriteLine($" Circle Perimeter is {circle.CalculatePerimeter()}");
+Console.WriteLine($" Rectangle Area ia {rectangle.CalculateArea()}");
+Console.WriteLine($" Rectangle Perimeter is {circle.CalculatePerimeter()}");
