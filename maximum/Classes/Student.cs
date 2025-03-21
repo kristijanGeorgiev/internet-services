c43adf8f7f3a2ae707bfa609cbb92f4a37ecbc82
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-21 19:28:53 +0100

    Add files via upload

---
+++ maximum/Classes/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum.Classes
+{
+    class Student
+    {
+        public int points { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public Student(int points, string Name, int Id)
+        {
+            this.points = points;
+            this.Name = Name;
+            this.Id = Id;
+        }
+    }
+}
---
+++ maximum/Program.cs
@@ -0,0 +1,3 @@
+﻿using maximum.Classes;
+Student student = new Student(72, "Kristijan", 1);
+Console.WriteLine($" Student Id {student.Id} Student Name {student.Name} Student Points {student.points}");
