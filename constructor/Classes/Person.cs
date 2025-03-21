b475893e173c796bfed585190f74dde1d2dbb101
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-21 18:30:01 +0100

    Add files via upload

---
+++ constructor/Classes/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor.Classes
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string Name)
+        {
+            this.Name = Name;
+        }
+        public Person()
+        {
+            Name = string.Empty;
+        }
+        public string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ constructor/Program.cs
@@ -0,0 +1,4 @@
+﻿using constructor.Classes;
+Person person = new Person();
+person.Name = "Kristijan";
+Console.WriteLine($" Hello, My Name is {person.Name}");
