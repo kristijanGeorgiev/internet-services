c7a4717985dfca03f8f58dae1f73c058e24b57b0
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-22 14:35:30 +0100

    Add files via upload

---
+++ StudentProfessor/Polymorphism/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProfessor.Polymorphism
+{
+    class Person
+    {
+        public int age { get; set; }
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+    }
+    class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm Studying");
+        }
+        public void ShowAge(int n)
+        {
+            Console.WriteLine($" My age is {n} years old");
+        }
+    }
+    class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm Explaining");
+        }
+    }
+}
---
+++ StudentProfessor/Program.cs
@@ -0,0 +1,9 @@
+﻿using StudentProfessor.Polymorphism;
+
+Person person = new Person();
+person.Greet();
+Student student = new Student();
+student.Study();
+student.ShowAge(22);
+Teacher teacher = new Teacher();
+teacher.Explain();
